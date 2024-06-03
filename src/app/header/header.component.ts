83e44f3497487432abb05487a8e26a234720aaa3
Andres Revollo <andres.revollo@ucb.edu.bo>
2024-06-03 15:34:30 -0400

    Se agrego el header cuando esta en vendedor-inicio

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CartService } from '../cart.service';
+import { NgIf, NgSwitch, NgSwitchCase } from '@angular/common';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule,],
+  imports: [RouterModule,NgIf,NgSwitchCase,NgSwitch],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
+  menuType: String = 'default';
   public totalitem = 0;
 
-  constructor(private cart: CartService) {}
+  constructor(private cart: CartService, private route:Router) {}
 
   ngOnInit(): void {
+    this.route.events.subscribe((val: any) => {
+      if (val.url) {
+        if (localStorage.getItem('vendedor') && val.url.includes('vendedor-inicio')) {
+          console.warn('this is seller area');
+          this.menuType = 'vendedor';
+        } else {
+          this.menuType = 'default';
+        }
+      }
+    });
     this.cart.getprouduct().subscribe(res => {
       this.totalitem = this.cart.getTotalItemCount();
     });
