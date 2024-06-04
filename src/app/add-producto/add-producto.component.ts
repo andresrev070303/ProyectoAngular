f730bdcc739e55798f106ea7b1551b26423552c3
Andres Revollo <andres.revollo@ucb.edu.bo>
2024-06-03 22:07:03 -0400

    Se agrego la interfaz de agregar producto

--- src/app/add-producto/add-producto.component.ts
+++ src/app/add-producto/add-producto.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-add-producto',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-producto.component.html',
   styleUrl: './add-producto.component.scss'
 })
-export class AddProductoComponent {
+export class AddProductoComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void {
+  }
 
+  submit(data:object){
+    console.warn(data);
+
+  }
 }
