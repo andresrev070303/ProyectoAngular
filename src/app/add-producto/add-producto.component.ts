35e5454b73db8b0b24a84394355642932dd4304e
Andres Revollo <andres.revollo@ucb.edu.bo>
2024-06-03 23:44:14 -0400

    Ahora se agrega un producto y se lanza un mensaje si el producto fue agregado correactamente

--- src/app/add-producto/add-producto.component.ts
+++ src/app/add-producto/add-producto.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ProductService } from '../services/product.service';
+import { product } from '../data-type';
 
 
 @Component({
@@ -11,12 +13,21 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './add-producto.component.scss'
 })
 export class AddProductoComponent implements OnInit {
-  constructor() { }
+  addProductMessage: string | undefined;
+  constructor(private product:ProductService) { }
   ngOnInit(): void {
   }
 
-  submit(data:object){
-    console.warn(data);
+  submit(data:product){
 
+    this.product.addProduct(data).subscribe((result)=>{
+      console.warn('result',result);
+      if(result){
+        this.addProductMessage='Se agrego el producto correctamente';
+      }
+      setTimeout(()=>{
+        this.addProductMessage=undefined;
+      },3000)
+    }) ;
   }
 }
